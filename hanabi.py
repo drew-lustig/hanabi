e3075ed0775b818fab986ba99ba4a2d508b617c2
Drew Lustig <drew.lustig@essenceglobal.com>
2020-05-28 14:02:33 -0400

    initial commit

---
+++ hanabi.py
@@ -0,0 +1,170 @@
+from random import shuffle
+
+
+class CardDeck(object):
+    variations = {
+        'classic': (
+            ['r', 'b', 'g', 'y', 'w'],
+            {1: 3, 2: 2, 3: 2, 4: 2, 5: 1}
+        )
+    }
+
+    def __init__(self, variation='classic'):
+        self._variation = variation
+        self.colors, self.numbers = self.variations[variation]
+        self.discarded = {color: [] for color in self.colors}
+        self.played = {color: 0 for color in self.colors}
+        self.deck = self.full_deck(variation=variation)
+        shuffle(self.deck)
+
+    @property
+    def variation(self):
+        return self._variation
+
+    @variation.setter
+    def variation(self, value):
+        if len(self.deck) < self.full_deck():
+            raise AttributeError('Cannot change variation after game has started.')
+        else:
+            self._variation = value
+            self.colors, self.numbers = self.variations[value]
+
+    @classmethod
+    def full_deck(cls, variation='classic'):
+        full_deck = []
+        colors, numbers = cls.variations[variation]
+        for color in colors:
+            for number, quantity in numbers.items():
+                full_deck.extend([(color, number)] * quantity)
+        return full_deck
+
+    @classmethod
+    def single_possibilities(cls, variation='classic'):
+        colors, numbers = cls.variations[variation]
+        return [set(colors), set(x for x in numbers.keys())]
+
+    @classmethod
+    def all_possibilities(cls, hand_size, variation='classic'):
+        return [
+            cls.single_possibilities(variation) for i in range(hand_size)
+        ]
+
+    @classmethod
+    def max_score(cls, variation='classic'):
+        colors, numbers = cls.variations[variation]
+        return len(colors) * len(numbers)
+
+
+class Player(object):
+
+    def __init__(self, name, hand):
+        self.name = name
+        self.hand = hand
+        self.possibilities = CardDeck.all_possibilities(len(hand))
+
+
+class Game(CardDeck):
+
+    def __init__(self, players=['Player 1', 'Player 2', 'Player 3'],
+                 variation='classic'):
+        CardDeck.__init__(self, variation)
+        self._tokens = 8
+        self._fuses = 0
+        self.score = 0
+        self.current_max = self.max_score(variation=variation)
+        if len(players) < 4:
+            self.hand_size = 5
+        else:
+            self.hand_size = 4
+        self.players = []
+        for player in players:
+            self.players.append(Player(player, self.deck[:self.hand_size]))
+            self.deck = self.deck[self.hand_size:]
+
+    @property
+    def tokens(self):
+        return self._tokens
+
+    @tokens.setter
+    def tokens(self, value):
+        if value >= 9:
+            raise ValueError('Cannot have more than 8 tokens.')
+        elif value < 0:
+            raise ValueError('Out of tokens')
+        else:
+            self._tokens = value
+
+    @property
+    def fuses(self):
+        return self._fuses
+
+    @fuses.setter
+    def fuses(self, value):
+        if value > 2:
+            raise ValueError('Game Over! Bomb went off.')
+        elif value < 0:
+            raise ValueError('Cannot have negative fuses.')
+        else:
+            self._fuses = value
+
+    def update_score(self):
+        self.score = sum(self.played.values(), 0)
+
+    def update_max(self, color, number):
+        quantity = self.numbers[number]
+        max_number = max(self.numbers.keys())
+        if self.discarded[color].count(number) == quantity:
+            deduction = number - max_number - 1
+            self.current_max += deduction
+
+    def remove_hint(self, player, index):
+        player.possibilities.pop(index)
+        player.possibilities.append(self.single_possibilities(variation=self.variation))
+
+    def hint(self, player, hint):
+        self.tokens += -1
+        if type(hint) == str:
+            index = 0
+        else:
+            index = 1
+        indices = [i for i, card in enumerate(player.hand) if card[index] == hint]
+        set_hint = set([hint])
+        for i in range(len(player.possibilities)):
+            if i in indices:
+                player.possibilities[i][index] = set_hint
+            else:
+                player.possibilities[i][index] = player.possibilities[i][index] - set_hint
+
+    def play(self, player, index):
+        color, number = player.hand.pop(index)
+        if number - self.played[color] == 1:
+            self.played[color] += 1
+            if number == 5 and self.tokens < 8:
+                self.tokens += 1
+            self.update_score()
+        else:
+            self.discarded[color].append(number)
+            self.fuses += 1
+            self.update_max(color, number)
+        player.hand.append(self.deck.pop())
+        self.remove_hint(player, index)
+
+    def discard(self, player, index):
+        self.tokens += 1
+        color, number = player.hand.pop(index)
+        self.discarded[color].append(number)
+        self.remove_hint(player, index)
+
+    def turn(self, player, choice, value, hint_player=None):
+        if choice == 'hint':
+            if player == hint_player:
+                raise ValueError('Cannot give yourself a hint! :)')
+            else:
+                self.hint(hint_player, value)
+        elif choice == 'play':
+            self.play(player, value)
+        elif choice == 'discard':
+            self.discard(player, value)
+        else:
+            raise ValueError('Must give a hint, play a card, or discard.')
+        return True
---
+++ tests/test_basic.py
@@ -0,0 +1,177 @@
+import unittest
+from functools import reduce
+
+import hanabi
+
+
+class TestDeck(unittest.TestCase):
+
+    def test_fullDeck(self):
+        each_color = 10
+        each_number = [15, 10, 10, 10, 5]
+        deck = hanabi.CardDeck()
+        full_deck = deck.full_deck()
+
+        # Check colors
+        for i in range(len(deck.colors)):
+            color = deck.colors[i]
+            matches = len(list(filter(
+                lambda x: True if x[0] == color else False, full_deck
+            )))
+            msg = f'Color: {color}'
+            self.assertEqual(each_color, matches, msg)
+
+        # Check numbers
+        numbers = list(set(deck.numbers))
+        for i in range(len(numbers)):
+            matches = len(list(filter(
+                lambda x: True if x[1] == numbers[i] else False, full_deck
+            )))
+            msg = f'Number: {numbers[i]}'
+            self.assertEqual(each_number[i], matches, msg)
+
+
+class TestPlayer(unittest.TestCase):
+
+    def test_possibilities(self):
+        game = hanabi.Game()
+        player = game.players[0]
+        possibilities = player.possibilities
+        self.assertEqual(
+            possibilities,
+            [
+                [set(game.colors), set([1, 2, 3, 4, 5])],
+                [set(game.colors), set([1, 2, 3, 4, 5])],
+                [set(game.colors), set([1, 2, 3, 4, 5])],
+                [set(game.colors), set([1, 2, 3, 4, 5])],
+                [set(game.colors), set([1, 2, 3, 4, 5])]
+            ]
+        )
+
+
+class TestGame(unittest.TestCase):
+
+    def test_gameStart(self):
+        tokens = 8
+        fuses = 0
+        hand_size = 5
+
+        game = hanabi.Game()
+        self.assertEqual(tokens, game.tokens, msg='tokens')
+        self.assertEqual(fuses, game.fuses, msg='fuses')
+        for player in game.players:
+            self.assertEqual(hand_size, len(player.hand), msg='hand size')
+        self.assertEqual(len(game.deck), len(game.full_deck())-15)
+
+    def test_hint(self):
+        game = hanabi.Game()
+        hand = [('r', 1), ('w', 4), ('w', 3), ('b', 1), ('y', 1)]
+
+        red_hint = [
+            [set(['r']), set(range(1, 6))],
+            [set(['b', 'g', 'y', 'w']), set(range(1, 6))],
+            [set(['b', 'g', 'y', 'w']), set(range(1, 6))],
+            [set(['b', 'g', 'y', 'w']), set(range(1, 6))],
+            [set(['b', 'g', 'y', 'w']), set(range(1, 6))]
+        ]
+        white_hint = [
+            [set(['r', 'b', 'g', 'y']), set(range(1, 6))],
+            [set(['w']), set(range(1, 6))],
+            [set(['w']), set(range(1, 6))],
+            [set(['r', 'b', 'g', 'y']), set(range(1, 6))],
+            [set(['r', 'b', 'g', 'y']), set(range(1, 6))]
+        ]
+        one_hint = [
+            [set(game.colors), set([1])],
+            [set(game.colors), set(range(2, 6))],
+            [set(game.colors), set(range(2, 6))],
+            [set(game.colors), set([1])],
+            [set(game.colors), set([1])]
+        ]
+
+        player1 = game.players[0]
+        player1.hand = hand
+        game.hint(player1, 'r')
+        self.assertEqual(red_hint, player1.possibilities)
+
+        player1.possibilities = hanabi.CardDeck.all_possibilities(len(hand))
+        game.hint(player1, 'w')
+        self.assertEqual(white_hint, player1.possibilities)
+
+        player1.possibilities = hanabi.CardDeck.all_possibilities(len(hand))
+        game.hint(player1, 1)
+        self.assertEqual(one_hint, player1.possibilities)
+
+        self.assertEqual(game.tokens, 5)
+        for i in range(5):
+            game.hint(player1, 1)
+        with self.assertRaises(ValueError):
+            game.hint(player1, 1)
+
+    def test_discard(self):
+        game = hanabi.Game()
+        player = game.players[0]
+        card = player.hand[0]
+        with self.assertRaises(ValueError):
+            game.discard(player, 0)
+        game.hint(player, 'g')
+        game.discard(player, 0)
+        discarded = game.discarded[card[0]][0]
+        self.assertEqual(card[1], discarded)
+        self.assertEqual(game.tokens, 8)
+        self.assertEqual(player.possibilities[-1],
+                         game.single_possibilities(variation=game.variation))
+
+    def test_play(self):
+        game = hanabi.Game()
+        player = game.players[0]
+        hand = [('r', 1), ('w', 4), ('w', 4), ('b', 5), ('y', 3)]
+        player.hand = hand
+
+        played = {'r': 1, 'g': 0, 'w': 0, 'b': 0, 'y': 0}
+        discarded = {'r': [], 'g': [], 'w': [], 'b': [], 'y': []}
+        game.play(player, 0)
+        self.assertEqual(game.played, played, 'playable')
+        self.assertEqual(game.discarded, discarded, 'playable')
+        self.assertEqual(game.score, 1, 'playable 1')
+        self.assertEqual(game.current_max, 25, 'playable')
+        self.assertEqual(game.tokens, 8, 'playable')
+        self.assertEqual(game.fuses, 0, 'playable')
+
+        discarded = {'r': [], 'g': [], 'w': [4], 'b': [], 'y': []}
+        game.play(player, 0)
+        self.assertEqual(game.played, played, 'unplayable 1')
+        self.assertEqual(game.discarded, discarded, 'unplayable 1')
+        self.assertEqual(game.current_max, 25, 'unplayable 1')
+        self.assertEqual(game.tokens, 8, 'unplayable 1')
+        self.assertEqual(game.fuses, 1, 'unplayable 1')
+
+        discarded = {'r': [], 'g': [], 'w': [4, 4], 'b': [], 'y': []}
+        game.play(player, 0)
+        self.assertEqual(game.played, played, 'unplayable 2')
+        self.assertEqual(game.discarded, discarded, 'unplayable 2')
+        self.assertEqual(game.current_max, 23, 'unplayable 2')
+        self.assertEqual(game.tokens, 8, 'unplayable 2')
+        self.assertEqual(game.fuses, 2, 'unplayable 2')
+
+        game.played['b'] = 4
+        game.tokens = 4
+        played = {'r': 1, 'g': 0, 'w': 0, 'b': 5, 'y': 0}
+        game.play(player, 0)
+        self.assertEqual(game.played, played, 'playable 5')
+        self.assertEqual(game.discarded, discarded, 'playable 5')
+        self.assertEqual(game.current_max, 23, 'playable 5')
+        self.assertEqual(game.tokens, 5, 'playable 5')
+        self.assertEqual(game.fuses, 2, 'playable 5')
+
+        discarded['y'] = [3]
+        score = 1
+        with self.assertRaises(ValueError):
+            game.play(player, 0)
+        self.assertEqual(game.played, played, 'unplayable 3')
+        self.assertEqual(game.discarded, discarded, 'unplayable 3')
+        self.assertEqual(game.score, 6, 'unplayable 3')
+        self.assertEqual(game.current_max, 23, 'unplayable 3')
+        self.assertEqual(game.tokens, 5, 'unplayable 3')
+        self.assertEqual(game.fuses, 2, 'unplayable 3')
+        
